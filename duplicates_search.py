ee162bb50e02b2c6710ca8f066cc0c4153eea2aa
olgalupuleac <olga_lupuleac@mail.ru>
2016-10-04 00:16:05 +0300

    Updated

--- duplicates_search.py
+++ duplicates_search.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import hashlib
+import collections
 
 
 def md5sum(file_path):
@@ -11,24 +12,18 @@ def md5sum(file_path):
     return hasher.hexdigest()
 
 
-dict_of_diff_files = dict()
-
-
-def collect_duplicates(file_path):
-    if md5sum(file_path) not in dict_of_diff_files.keys():
-        dict_of_diff_files[md5sum(file_path)] = [os.path.relpath(
-                                                 file_path, top_dir)]
-    else:
-        dict_of_diff_files[md5sum(file_path)].append(
-                          os.path.relpath(file_path, top_dir))
-    return dict_of_diff_files
+hash_to_files = collections.defaultdict(list)
 
 
 top_dir = sys.argv[1]
 for root, _, files in os.walk(top_dir):
     for filename in files:
         if not filename.startswith('.') and not filename.startswith('~'):
-            collect_duplicates(os.path.join(root, filename))
+            file_path = os.path.join(root, filename)
+            if not (os.path.islink(file_path)):
+                hsh = md5sum(file_path)
+                hash_to_files[hsh].append(os.path.relpath(file_path, top_dir))
 
-for list_of_duplicates in dict_of_diff_files.values():
-        print(':'.join(map(str, list_of_duplicates)))
+for duplicates in hash_to_files.values():
+    if len(duplicates) > 1:
+        print(':'.join(duplicates))
